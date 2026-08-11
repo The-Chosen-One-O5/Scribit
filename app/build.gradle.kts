223d6df20d9b218afba4f12d996fda36cd24d494
The-Chosen-One-O5 <shakthinathan2008@gmail.com>
2026-08-11 13:51:16 +0530

    Update Scribit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 
     buildFeatures {
         compose = true
@@ -45,6 +42,13 @@ android {
 
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
+
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
